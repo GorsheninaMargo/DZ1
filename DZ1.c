0432bfc8af81442589a3bc1c1970d31a7c823e06
GorsheninaMargarita <gorshenina23032303@mail.ru>
2021-09-23 16:17:57 +0300

    Commit1

--- DZ1.c
+++ DZ1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hello, World!");
-    return 0;
+void main(){
+    int c[64];
+    int i=0, x=0;
+    scanf("%d", &x);
+    while(x>1){
+        c[i]=x%2;
+        x=x/2;
+        i++;
+    }
+    c[i]=x;
+    for(i;i>=0;i--) printf("%d", c[i]);
 }
